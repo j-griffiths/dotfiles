be5c1cd77c7e9eb9cb3ee1adf6c5c55ec340c796
Jonathan Griffiths <74262798+j-griffiths@users.noreply.github.com>
2024-05-17 19:15:00 +0100

    Remove install script

--- install-tools.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-
-zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-ln -s ~/git/dotfiles/zsh/.zshrc ~/.zshrc
-
